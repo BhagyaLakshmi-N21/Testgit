081691ca526bdcb810816eaf809d579619c1fcca
BhagyaLakshmi-N21 <bhagya.961996@gmail.com>
2020-04-30 11:54:59 +0530

    adding folderstructure

---
+++ newjsfiles/div.js
@@ -0,0 +1,10 @@
+let a;
+let b;
+
+function div(a , b){
+    return a/b ;
+}
+
+let c=div(50,0);
+
+console.log(c);
---
+++ newjsfiles/summ.js
@@ -0,0 +1,10 @@
+let a;
+let b;
+
+function sum(a , b){
+    return a+b ;
+}
+
+let c=sum(56,85);
+
+console.log(c);
